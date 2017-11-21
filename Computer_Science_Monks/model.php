de2ab373ff9f893148abb4de012e13feb541990b
jtdarkly <mintchaijava@gmail.com>
2017-11-21 02:14:55 +0000

    oops

--- Computer_Science_Monks/model.php
+++ Computer_Science_Monks/model.php
@@ -83,7 +83,7 @@ ORDER BY ";
      * login information.  controller.php uses this method.
      */
     public static function connectDB() {
-        require_once 'login.php';
+        require_once '/home/ubuntu/.login/login.php';
             // PDO connection
             self::$conn = new PDO($device, $user, $password);
     } // end function connectDB
